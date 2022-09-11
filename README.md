70535539bb495e3996237798981cc09809853654
hyune-c <choi8608@gmail.com>
2022-09-11 21:31:37 +0900

    :wrench: set spring boot

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # java-spring-init
+
 자바 스프링으로 프로젝트 시작하기
---
+++ src/test/java/com/example/javaspringinit/JavaSpringInitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringInitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
